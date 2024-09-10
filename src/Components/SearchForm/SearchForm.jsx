2910da27b905daf6e599837450db6c6f961697de
Alona Zabolotskykh <alenka.polishchuk@gmail.com>
2024-09-10 19:18:02 +0300

    form

--- src/Components/SearchForm/SearchForm.jsx
+++ src/Components/SearchForm/SearchForm.jsx
@@ -1,1 +1,19 @@
-export default function SearchForm() {}
+import { useSearchParams } from "react-router-dom";
+
+export default function SearchForm() {
+  const [params, setParams] = useSearchParams();
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    params.set("query", evt.target.elements.query.value);
+    setParams(params);
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="query" />
+        <button type="submit">Search</button>
+      </form>
+    </>
+  );
+}
--- src/Pages/MoviesPage/MoviesPage.jsx
+++ src/Pages/MoviesPage/MoviesPage.jsx
@@ -1,1 +1,40 @@
-export default function MoviesPage() {}
+import { useEffect, useState } from "react";
+import { getSearchMovie } from "../../API/SearchMovie";
+import SearchForm from "../../Components/SearchForm/SearchForm";
+import { useSearchParams } from "react-router-dom";
+import Loader from "../../Components/Loader/Loader";
+import Error from "../../Components/Error/Error";
+
+export default function MoviesPage() {
+  const [movie, setMovie] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const [params] = useSearchParams();
+  const query = params.get("query");
+
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+    async function fetchData() {
+      try {
+        const data = await getSearchMovie(query);
+        setMovie(data);
+      } catch {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, [query]);
+
+  return (
+    <>
+      <SearchForm />
+      {loading && <Loader />}
+      {error && <Error />}
+    </>
+  );
+}
